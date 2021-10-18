2843d326864cad469a4bbe02b981fae81ce98f7e
danield770 <dddavid2@gmail.com>
2021-10-18 15:57:24 +0300

    implement grid component, add some styling

--- src/Grid.js
+++ src/Grid.js
@@ -1,22 +1,28 @@
 import React from 'react';
+import './Grid.css';
 
 const Grid = ({ config, data }) => (
-  <table>
+  <table className='grid' style={{ '--numCols': config.length }}>
     <thead>
-    <tr>
-      <th>Col 1</th>
-      <th>Col 2</th>
-    </tr>
+      <tr>
+        {config.map(({ title, field }) => (
+          <th key={field}>{title}</th>
+        ))}
+      </tr>
     </thead>
     <tbody>
-    <tr>
-      <td>Data 1</td>
-      <td>Data 2</td>
-    </tr>
-    <tr>
-      <td>Data 1</td>
-      <td>Data 2</td>
-    </tr>
+      {data.map((data) => (
+        <tr key={data.imdbID}>
+          <td>{data[config[0].field]}</td>
+          <td>{data[config[1].field]}</td>
+          <td>{data[config[2].field]}</td>
+          <td>
+            {React.createElement(config[3].component, {
+              data: data[config[3].field],
+            })}
+          </td>
+        </tr>
+      ))}
     </tbody>
   </table>
 );
